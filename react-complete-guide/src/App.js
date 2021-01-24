c48da997525bbc222302ac5751bb95ea7864be52
mackilroy007 <mackilroy007@gmail.com>
2021-01-24 19:46:05 +0100

    defining setState within the eventhandler to update state on click

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,15 +5,22 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28},
-      {name: 'Manu', age: 15},
-      {name: 'Alexa', age: 22}
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 15 },
+      { name: 'Alexa', age: 22 }
     ]
   }
 
-  switchNameHandler = () =>{
-    console.log('Was clicked!');
-  } 
+  switchNameHandler = () => {
+    // console.log('Was clicked!');
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 25 },
+        { name: 'Alexa', age: 30 }
+      ]
+    })
+  }
 
   render() {
     return (
@@ -21,9 +28,9 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies are: Coding React</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // this is how it compiles the above text
