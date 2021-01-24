cdce4d4f2bdb2a943444af76b99580c95c9fb0a8
mackilroy007 <mackilroy007@gmail.com>
2021-01-24 16:10:49 +0100

    adding state within App.js, eliminating the hardcoded name and age code

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28},
+      {name: 'Manu', age: 15},
+      {name: 'Alexa', age: 22}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name='MAX' age='28'/>
-        <Person name='Manu' age='15'>My hobbies are: Coding React</Person>
-        <Person name='Alexa' age='22'/>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies are: Coding React</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // this is how it compiles the above text
