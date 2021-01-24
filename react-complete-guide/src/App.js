99295a18b6ef0fde3a8241ef33484d0a3cd85934
mackilroy007 <mackilroy007@gmail.com>
2021-01-24 15:38:46 +0100

    adding a Person Component and including dynamic JavaScript content within the static <p> tag

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person/>
+        <Person/>
+        <Person/>
+      </div>
+    );
+    // this is how it compiles the above text
+    // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Hi, I\'m a React App!!'));
+  }
 }
 
 export default App;
