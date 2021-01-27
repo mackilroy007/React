e1bb61556b1d63be20f9769c698e667946586d4a
mackilroy007 <mackilroy007@gmail.com>
2021-01-27 22:14:58 +0100

    changing onClick handler location from the <p> to the <div> to make the click action more accurate

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -5,9 +5,9 @@ import './Person.css';
 
 const person = (props) => {
     return (
-        <div className='Person'>
+        <div className='Person' onClick={props.click}>
             {/* props.click refers to the App.js custom property event handler */}
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p >I'm {props.name} and I am {props.age} years old!</p>
             {/* displays any code that is inbetween the <Person> tags in App. */}
             <p>{props.children}</p>
             {/* props.changed also refers to the custom property */}
